b25f9d5267519abdbde263fe658b9c4d36ad7ac0
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-02-13 14:13:14 +0100

    inizio creazione e inserimento contenuti funzione CRUD (Read) index e show

--- app/Http/Controllers/Admin/ComicsController.php
+++ app/Http/Controllers/Admin/ComicsController.php
@@ -7,5 +7,11 @@ use Illuminate\Http\Request;
 
 class ComicsController extends Controller
 {
-    //
+    public function index(){
+        return view("index");
+    }
+
+    public function show($id){
+        return view("show");
+    }
 }
