9895437bc77502e3e0faf0028c9dacef0e94f2bb
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-02-16 14:26:59 +0100

    validation per update e stampo errori nel form

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -9,31 +9,94 @@
             @method('PUT')
             <div class="mb-3">
                 <label for="formGroupExampleInput" class="form-label">title</label>
-            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="" value="{{$comic->title}}" name="title">
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="" value="{{old('title') ?? $comic->title}}" name="title">
+                @if ($errors->has('title'))
+                <div class="alert alert-danger mt-3">
+                    <ul class="">
+                        @foreach ($errors->get('title') as $error)
+                        <li>{{$error}}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
             </div>
             <div class="mb-3">
-            <label for="formGroupExampleInput2" class="form-label">description</label>
-               <textarea name="description" class="form-control">{{$comic->description}}</textarea>
+                <label for="formGroupExampleInput2" class="form-label">description</label>
+                <textarea name="description" class="form-control">{{old('description') ?? $comic->description}}</textarea>
+                @if ($errors->has('description'))
+                <div class="alert alert-danger mt-3">
+                    <ul class="">
+                        @foreach ($errors->get('description') as $error)
+                        <li>{{$error}}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
             </div>
             <div class="mb-3">
                 <label for="formGroupExampleInput2" class="form-label">thumb</label>
-                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="" value="{{$comic->thumb}}" name="thumb">
+                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="" value="{{old('thumb') ?? $comic->thumb}}" name="thumb">
+                @if ($errors->has('thumb'))
+                <div class="alert alert-danger mt-3">
+                    <ul class="">
+                        @foreach ($errors->get('thumb') as $error)
+                        <li>{{$error}}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
             </div>
             <div class="mb-3">
                 <label for="formGroupExampleInput2" class="form-label">price</label>
-                <input type="number" class="form-control" id="formGroupExampleInput2" placeholder="" value="{{$comic->price}}" name="price">
+                <input type="number" class="form-control" id="formGroupExampleInput2" placeholder="" value="{{old('price') ?? $comic->price}}" name="price">
+                @if ($errors->has('price'))
+                <div class="alert alert-danger mt-3">
+                    <ul class="">
+                        @foreach ($errors->get('price') as $error)
+                        <li>{{$error}}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
             </div>
             <div class="mb-3">
                 <label for="formGroupExampleInput2" class="form-label">series</label>
-                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="" value="{{$comic->series}}" name="series">
+                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="" value="{{old('series') ?? $comic->series}}" name="series">
+                @if ($errors->has('series'))
+                <div class="alert alert-danger mt-3">
+                    <ul class="">
+                        @foreach ($errors->get('series') as $error)
+                        <li>{{$error}}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
             </div>
             <div class="mb-3">
                 <label for="formGroupExampleInput2" class="form-label">sale date</label>
-                <input type="date" class="form-control" id="formGroupExampleInput2" placeholder="" value="{{$comic->sale_date}}" name="sale_date">
+                <input type="date" class="form-control" id="formGroupExampleInput2" placeholder="" value="{{old('sale_date') ?? $comic->sale_date}}" name="sale_date">
+                @if ($errors->has('sale_date'))
+                <div class="alert alert-danger mt-3">
+                    <ul class="">
+                        @foreach ($errors->get('sale_date') as $error)
+                        <li>{{$error}}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
             </div>
             <div class="mb-3">
                 <label for="formGroupExampleInput2" class="form-label">type</label>
-                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="" value="{{$comic->type}}" name="type">
+                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="" value="{{old('type') ?? $comic->type}}" name="type">
+                @if ($errors->has('type'))
+                <div class="alert alert-danger mt-3">
+                    <ul class="">
+                        @foreach ($errors->get('type') as $error)
+                        <li>{{$error}}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
             </div>
             <button type="submit">Invio</button>
         </form>
